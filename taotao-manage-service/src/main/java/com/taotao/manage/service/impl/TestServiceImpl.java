8da320f216fbae359434083915dbb77decfc196c
mcgrady2030524 <luzhenjie2015@163.com>
2018-01-09 23:02:59 +0800

    初次提交

---
+++ taotao-manage-service/src/main/java/com/taotao/manage/service/impl/TestServiceImpl.java
@@ -0,0 +1,23 @@
+package com.taotao.manage.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.manage.mapper.TestMapper;
+import com.taotao.manage.service.TestService;
+
+@Service
+public class TestServiceImpl implements TestService{
+	
+	//注入mapper
+	@Autowired
+	private TestMapper testMapper;
+
+	@Override
+	public String queryCurrentDate() {
+		
+		return testMapper.queryCurrentDate();
+	}
+	
+
+}
