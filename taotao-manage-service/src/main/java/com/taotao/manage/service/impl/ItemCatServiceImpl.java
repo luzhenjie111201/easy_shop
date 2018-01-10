6f91aad7104935c067e75686abaccfd8d29a492a
mcgrady2030524 <luzhenjie2015@163.com>
2018-01-10 21:31:46 +0800

    提交

--- taotao-manage-service/src/main/java/com/taotao/manage/service/impl/ItemCatServiceImpl.java
+++ taotao-manage-service/src/main/java/com/taotao/manage/service/impl/ItemCatServiceImpl.java
@@ -1,10 +1,8 @@
 package com.taotao.manage.service.impl;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.github.pagehelper.PageHelper;
 import com.taotao.manage.mapper.ItemCatMapper;
 import com.taotao.manage.pojo.ItemCat;
--- taotao-manage-web/src/main/java/com/taotao/manage/controller/ItemCatController.java
+++ taotao-manage-web/src/main/java/com/taotao/manage/controller/ItemCatController.java
@@ -1,7 +1,6 @@
 package com.taotao.manage.controller;
 
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
